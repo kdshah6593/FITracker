413f84464b189d5554638277890a0b3d412d8833
Kunal <kdshah6593@gmail.com>
2021-01-01 16:40:26 -0500

    skeleton crequest and controller actions for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,23 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+  # login
+  get "/login" do
+    #erb a login form
   end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+  post "/login" do
+    redirect "/workouts"
   end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
+  # sign up
+  get "/sign-up" do
+    #erb a sign-up form
   end
 
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
+  post "/sign-up" do
+    #creates a user
+    #login the user with sessions
+    redirect "/workouts"
   end
-
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
-
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
-
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
+  
 end
