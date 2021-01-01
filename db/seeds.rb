a64d8cb380afffed4592a9774b7c9c2c6d94864c
Kunal <kdshah6593@gmail.com>
2021-01-01 17:44:56 -0500

    reseed data, workout views page error for username fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+User.delete_all
+Workout.delete_all
+Exercise.delete_all
+
 john = User.create(username: "johnsmith", password_digest: "password", first_name: "John", last_name: "Smith", email: "johnsmith@email.com")
 jane = User.create(username: "janedoe", password_digest: "password", first_name: "Jane", last_name: "Doe", email: "janedoe@email.com")
 
@@ -6,10 +10,15 @@ w1 = Workout.create(title: "Cardiac Hill", workout_type: "Cardio" )
 w2 = Workout.create(title: "Legs All Day", workout_type: "Strength" )
 
 
-w1.exercises.create(title: "Run", sets: 1, reps: 1, distance: 5, duration: 30)
-w2.exercises.create(title: "Squats", sets: 3, reps: 10)
-w2.exercises.create(title: "Calf Raises", sets: 3, reps: 10)
-w2.exercises.create(title: "Leg Press", sets: 3, reps: 10)
+e1 = Exercise.create(title: "Run", sets: 1, reps: 1, distance: 5, duration: 30)
+e2 = Exercise.create(title: "Squats", sets: 3, reps: 10)
+e3 = Exercise.create(title: "Calf Raises", sets: 3, reps: 10)
+e4 = Exercise.create(title: "Leg Press", sets: 3, reps: 10)
 
-w1.user = john
-w2.user = jane
+w1.exercises << e1
+w2.exercises << e2
+w2.exercises << e3
+w2.exercises << e4
+
+john.workouts << w1
+jane.workouts << w2
