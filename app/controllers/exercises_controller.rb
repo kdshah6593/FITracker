6bac20fc000ca6571c6d083a79f886641ad27a79
Kunal <kdshah6593@gmail.com>
2021-01-02 17:16:09 -0500

    controller action for adding new exercise

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,14 +1,16 @@
 class ExercisesController < ApplicationController
 
   #Create
-    #New Exercise - Form GET
     get "/:username/workouts/:id/exercises/new" do
+      @workout = Workout.find(params[:id])
       erb :"/exercises/new.html"
     end
 
-    #Add Exercise POST
     post "/:username/workouts/:id/exercises" do
-      redirect "/:username/workouts/:id"
+      @workout = Workout.find(params[:id])
+      @workout.exercises.create(params[:exercise])
+
+      redirect "/#{params[:username]}/workouts/#{params[:id]}"
     end
 
   #Edit
@@ -19,12 +21,16 @@ class ExercisesController < ApplicationController
 
     #Update the exercise - PATCH
     patch "/:username/workouts/:id/exercises/" do
+      
+      
       redirect "/:username/workouts/:id"
     end
 
   #Delete
     #Delete a exercise
     delete "/workouts/username/workout_id/exercises/:id" do
+      
+      
       redirect "/:username/workouts/:id"
     end
   
