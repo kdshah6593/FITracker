980df92af74c8efd3199812a2e87786da24071cc
Kunal <kdshah6593@gmail.com>
2021-01-02 18:20:26 -0500

    user auth for exercise requests

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,38 +2,81 @@ class ExercisesController < ApplicationController
 
   #Create
     get "/:username/workouts/:id/exercises/new" do
-      @workout = Workout.find(params[:id])
-      erb :"/exercises/new.html"
+      if logged_in?
+        @workout = Workout.find(params[:id])
+        if @workout && @workout.user == current_user
+          erb :"/exercises/new.html"
+        else
+          redirect "/#{@workout.user.username}/workouts/#{@workout.id}"
+        end
+      else
+        redirect "/login"
+      end
     end
 
     post "/:username/workouts/:id/exercises" do
-      @workout = Workout.find(params[:id])
-      @workout.exercises.create(params[:exercise])
-
-      redirect "/#{params[:username]}/workouts/#{params[:id]}"
+      if logged_in?
+        if !params[:exercise][:title].empty?
+          @workout = Workout.find(params[:id])
+          @workout.exercises.create(params[:exercise])
+          redirect "/#{params[:username]}/workouts/#{params[:id]}"
+        else
+          @error = "Exercise must include at least a title"
+          redirect "/#{params[:username]}/workouts/#{params[:id]}/edit"
+        end
+      else
+        redirect "/login"
+      end
     end
 
   #Edit
     get "/exercises/:id/edit" do
-      @exercise = Exercise.find(params[:id])
-      erb :"/exercises/edit.html"
+      if logged_in?
+        @exercise = Exercise.find(params[:id])
+        if @exercise && @exercise.workout.user == current_user
+          erb :"/exercises/edit.html"
+        else
+          redirect "/workouts"
+        end
+      else
+        redirect "/login"
+      end
     end
 
     patch "/exercises/:id" do
-      @exercise = Exercise.find(params[:id])
-      @exercise.update(params[:exercise])
-      @workout = Workout.find(@exercise.workout_id)
+      if logged_in?
+        if params[:exercise][:title] == ""
+          redirect "exercises/#{params[:id]}/edit"
+        else
+          @exercise = Exercise.find(params[:id])
+          if @exercise && @exercise.workout.user == current_user
+            @exercise.update(params[:exercise])
+            @workout = Workout.find(@exercise.workout_id)
       
-      redirect "/#{@workout.user.username}/workouts/#{@workout.id}"
+            redirect "/#{@workout.user.username}/workouts/#{@workout.id}"
+          else
+            redirect "/workouts"
+          end
+        end
+      else
+        redirect "/login"
+      end
     end
 
   #Delete
     get "/exercises/:id/delete" do
-      @exercise = Exercise.find(params[:id])
-      @workout = Workout.find(@exercise.workout_id)
-      @exercise.destroy
-      redirect "/#{@workout.user.username}/workouts/#{@workout.id}"
+      if logged_in?
+        @exercise = Exercise.find(params[:id])
+        if @exercise && @exercise.workout.user == current_user
+          @workout = Workout.find(@exercise.workout_id)
+          @exercise.destroy
+          redirect "/#{@workout.user.username}/workouts/#{@workout.id}"
+        end
+        redirect "/workouts"
+      else
+        redirect "/login"
+      end
     end
-  
+
 end
 
