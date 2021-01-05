b68de7936a75738874a33bbc8d6c4563597868cc
Kunal <kdshah6593@gmail.com>
2021-01-05 18:28:04 -0500

    fixed error with editing workout

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -61,7 +61,7 @@ class WorkoutsController < ApplicationController
 
     patch "/:username/workouts/:id" do
       if logged_in?
-        if params[:workout][:title] == "" || params[:workout].has_key(:workout_type) == false
+        if params[:workout][:title] == "" || params[:workout].has_key?(:workout_type) == false
           redirect "/#{params[:username]}/workouts/#{params[:id]}/edit"
         else
           @workout = Workout.find(params[:id])
